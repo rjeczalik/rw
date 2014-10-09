68b265167f79af39bd1d8e37b4acd3a5485ba307
Rafal Jeczalik <rjeczalik@gmail.com>
2014-10-09 19:11:10 +0200

    Add more test-cases for WaitingWriter.Write

--- waiting_test.go
+++ waiting_test.go
@@ -1,32 +1,53 @@
 package rw
 
 import (
+	"io"
 	"io/ioutil"
 	"testing"
 	"time"
 )
 
-func TestWaitWriter(t *testing.T) {
-	cases := map[string][]string{
-		"foo": {"opasd", "asdasdf", "osdfsdf", "rtyfoot"},
-		"bar": {"sdfba", "sdfswe34", "w3psgb", "ar2al"},
-		"baz": {"alqlqpqp102bBBAZ", "baz"},
-		"qux": {"quxpwo300ls,"},
-		"quu": {"dldl20slw", "wejznmqo10f8w0-ak", "04j0qm204n2", "ofg0qkquu"},
-		"209skswkwprt,mxg;dfglsp": {"a", "b", "209skswk", "wprt,mx", "g;dfglsp"},
+var cases = map[string][]string{
+	"foo": {"opasd", "asdasdf", "osdfsdf", "rtyfoot"},
+	"bar": {"sdfba", "sdfswe34", "w3psgb", "ar2al"},
+	"baz": {"alqlqpqp102bBBAZ", "baz"},
+	"qux": {"quxpwo300ls,"},
+	"quu": {"dldl20slw", "wejznmqo10f8w0-ak", "04j0qm204n2", "ofg0qkquu"},
+	"209skswkwprt,mxg;dfglsp": {"a", "b", "209skswk", "wprt,mx", "g;dfglsp"},
+}
+
+func write(w io.Writer, s []string) {
+	for _, s := range s {
+		if _, err := w.Write([]byte(s)); err != nil {
+			panic(err)
+		}
 	}
+}
+
+func TestWaitWriter(t *testing.T) {
 	for msg, parts := range cases {
 		w := WaitWriter(ioutil.Discard, []byte(msg))
-		go func() {
-			for _, s := range parts {
-				if _, err := w.Write([]byte(s)); err != nil {
-					panic(err)
-				}
-			}
-		}()
+		go write(w, parts)
 		if err := w.Wait(time.Second); err != nil {
 			t.Errorf("want w.Wait(...)=nil; got %v", err)
 			continue
+		}
+	}
+}
+
+func TestWaitWriterTimeout(t *testing.T) {
+	for msg, parts := range cases {
+		w := WaitWriter(ioutil.Discard, []byte(msg+"must timeout"))
+		err := make(chan error)
+		go write(w, parts)
+		go func() { err <- w.Wait(20 * time.Millisecond) }()
+		select {
+		case err := <-err:
+			if err == nil {
+				t.Errorf("want err != nil (msg=%s)", msg)
+			}
+		case <-time.After(time.Second):
+			t.Errorf("test has timed out (msg=%s)", msg)
 		}
 	}
 }
