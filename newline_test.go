c1a8dacc7fbd8d29acc6296598adaba60b57a8f2
Paweł Błaszczyk <blaszczykpb@gmail.com>
2014-10-17 22:26:26 +0200

    newLineWriter: Run always windows & unix tests.

--- newline_test.go
+++ newline_test.go
@@ -3,41 +3,64 @@ package rw
 import (
 	"bytes"
 	"io"
-	"runtime"
 	"strings"
 	"testing"
 )
 
-func TestNewLineWriter(t *testing.T) {
+func TestNewLineWriterWindows(t *testing.T) {
 	cases := [...]struct {
 		s string
-		w string
-		u string
+		r string
 	}{
-		{"dd\n", "dd\r\n", "dd\n"},
-		{"", "", ""},
-		{"\r\ni", "\r\ni", "\ni"},
-		{"\ndd\n", "\r\ndd\r\n", "\ndd\n"},
-		{"\n", "\r\n", "\n"},
-		{"\r", "\r", "\r"},
-		{"a\r\n", "a\r\n", "a\n"},
+		{"dd\n", "dd\r\n"},
+		{"", ""},
+		{"\r\ni", "\r\ni"},
+		{"\ndd\n", "\r\ndd\r\n"},
+		{"\n", "\r\n"},
+		{"\r", "\r"},
+		{"a\r\n", "a\r\n"},
 	}
 	for i, cas := range cases {
 		var buf bytes.Buffer
-		wr := NewLineWriter(&buf)
+		wr := &newLineWriter{&buf, winNewLine}
 		n, err := io.Copy(wr, strings.NewReader(cas.s))
 		if err != nil {
 			t.Errorf("want err=nil; got %v (i=%d)", err, i)
 			continue
 		}
-		var w string
-		if runtime.GOOS == "windows" {
-			w = cas.w
-		} else {
-			w = cas.u
+		if got := buf.String(); got != cas.r {
+			t.Errorf("want got=%q; got %q (i=%d)", cas.r, got, i)
+			continue
 		}
-		if got := buf.String(); got != w {
-			t.Errorf("want got=%q; got %q (i=%d)", w, got, i)
+		if want := len(cas.s); int(n) != want {
+			t.Errorf("want n=%d; got %d (i=%d)", want, n, i)
+		}
+	}
+}
+
+func TestNewLineWriterUnix(t *testing.T) {
+	cases := [...]struct {
+		s string
+		r string
+	}{
+		{"dd\n", "dd\n"},
+		{"", ""},
+		{"\r\ni", "\ni"},
+		{"\ndd\n", "\ndd\n"},
+		{"\n", "\n"},
+		{"\r", "\r"},
+		{"a\r\n", "a\n"},
+	}
+	for i, cas := range cases {
+		var buf bytes.Buffer
+		wr := &newLineWriter{&buf, unixNewLine}
+		n, err := io.Copy(wr, strings.NewReader(cas.s))
+		if err != nil {
+			t.Errorf("want err=nil; got %v (i=%d)", err, i)
+			continue
+		}
+		if got := buf.String(); got != cas.r {
+			t.Errorf("want got=%q; got %q (i=%d)", cas.r, got, i)
 			continue
 		}
 		if want := len(cas.s); int(n) != want {
